e6fbb604abad5aec156f72fd87bf1ea4b786b1f4
Raymond Tan <raymond.tan@intel.com>
2021-04-24 12:55:44 -0700

    assignment_001: Update knn-classification
    Signed-off-by: Raymond Tan <raymond.tan@intel.com>

--- assignment_001/.ipynb_checkpoints/classification-checkpoint.cpp
+++ assignment_001/.ipynb_checkpoints/classification-checkpoint.cpp
@@ -11,14 +11,15 @@
 #include <unistd.h>
 #include <ctime>
 
-#define DEBUG 0
-
-#define BLOCK_SIZE 256
-
 using namespace sycl;
 
-static const size_t N = 268435456; // sample size 2^28
-static const size_t power = 28; // power for Bitonic Sort
+#define DEBUG 1
+#define BLOCK_SIZE 256
+#define DEVICE_HOST 0
+#define DEVICE_GPU 1
+#define DEVICE_CPU 2
+
+static const size_t default_power = 8; // power for Bitonic Sort
 static const size_t K = 5; // k-value
 
 /*
@@ -34,10 +35,11 @@ Initialize K to your chosen number of neighbors
 8. If classification, return the mode of the K labels
  */
 
-void ParallelBitonicSort(uint32_t data_gpu[], int n, queue &q) {
+void ParallelBitonicSort(uint32_t data_gpu[], uint32_t index[], int n, queue &q) {
   // n: the exponent used to set the array size. Array size = power(2, n)
   int size = pow(2, n);
   uint32_t *a = data_gpu;
+  uint32_t *b = index;
 
   // step from 0, 1, 2, ...., n-1
   for (int step = 0; step < n; step++) {
@@ -79,6 +81,10 @@ void ParallelBitonicSort(uint32_t data_gpu[], int n, queue &q) {
               uint32_t temp = a[i];
               a[i] = a[swapped_ele];
               a[swapped_ele] = temp;
+                
+              uint32_t temp_index = b[i];
+              b[i] = b[swapped_ele];
+              b[swapped_ele] = temp_index;
             }
           }
         });
@@ -87,10 +93,6 @@ void ParallelBitonicSort(uint32_t data_gpu[], int n, queue &q) {
     }  // end stage
   }    // end step
 }
-
-#define DEVICE_HOST 0
-#define DEVICE_GPU 1
-#define DEVICE_CPU 2
 
 class custom_selector : public device_selector {
 public:
@@ -121,10 +123,12 @@ private:
 
 int main(int argc, char* argv[]) {  
     int opt;
+    int power = default_power;
+    int N = 0;
     int device = DEVICE_CPU;
     std::time_t start, end;
 
-    while((opt = getopt(argc, argv, ":d:")) != -1) {
+    while((opt = getopt(argc, argv, "p:d:")) != -1) {
         switch(opt)
         {
             case 'd':
@@ -136,11 +140,15 @@ int main(int argc, char* argv[]) {
                     std::cout << "Using CPU for DPCPP" << std::endl;
                 }
                 break;
+            case 'p':
+                power = atoi(optarg);
+                break;
             case ':':
                 std::cout << "Missing argument for -d" << std::endl;
             case '?':
             default:
                 std::cout << "Use -d [cpu|gpu] to determine which device custom selector will choose" << std::endl;
+                std::cout << "Use -p <power_int_value> to determine N (2 ^ power)" << std::endl;
                 return -1;
         }
     }
@@ -148,7 +156,10 @@ int main(int argc, char* argv[]) {
     custom_selector selector(device);
     queue q(selector);
     
+    N = 1 << power;
+    
     std::cout << "Device : " << q.get_device().get_info<info::device::name>() << std::endl;
+    std::cout << "N : " << N << " = 2^" << power << std::endl;
     srand(time(NULL));
     
     std::cout << "N size : " << N << std::endl;
@@ -163,22 +174,11 @@ int main(int argc, char* argv[]) {
         sample_index[i] = i;
         sample_age[i] = rand() % 70 + 20; // age 20 - 90
         sample_choice[i] = rand() % 2; 
-#if DEBUG  
-        printf("Age: %u Likes : %d \n", sample_age[i], sample_choice[i]);
-#endif
     }
     
     /* Random input */
     uint32_t input_age = rand() % 70 + 20;
     printf("Random Age: %u  \n", input_age);
-
-#if 0
-    /* Distance calculation -> Host Only (can be parallelized) */
-    for (int i = 0; i < N; i++) {
-        calc_distance[i] = abs((int)sample_age[i] - (int)input_age);
-        printf("Age: %u, Choice: %d, Dist: %d\n", sample_age[i], sample_choice[i], calc_distance[i]);
-    }
-#endif
     
     start = std::time(nullptr);
     
@@ -187,34 +187,46 @@ int main(int argc, char* argv[]) {
         calc_distance[i] = abs((int)sample_age[i] - (int)input_age);
     }).wait();
 
-#if 1
+#if DEBUG
     printf("Before Sort:\n");
     /* Sorted distance */
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 5; i++) {
         printf("Age: %u, Choice: %d, Dist: %d\n", sample_age[sample_index[i]], sample_choice[sample_index[i]], calc_distance[i]);
     }
 #endif
     
-    /* Using quicksort on the distance */
-    //quickSort(calc_distance, sample_index, 0, N-1);
-    
     /* Using bitonicsort on the distance */
-    ParallelBitonicSort(calc_distance, power, q);
+    ParallelBitonicSort(calc_distance, sample_index, power, q);
     
     end = std::time(nullptr);
     
-#if 1
+#if DEBUG
     printf("After Sort:\n");
     /* Sorted distance */
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 5; i++) {
         printf("Age: %u, Choice: %d, Dist: %d\n", sample_age[sample_index[i]], sample_choice[sample_index[i]], calc_distance[i]);
+    }
+    
+    printf("calc_distance distribution...\n");
+    uint32_t temp = calc_distance[0];
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        if (temp != calc_distance[i]) {
+            printf("Dist: %d, count = %d\n", temp, count);
+            temp = calc_distance[i];
+            count = 1;
+        } else {
+            count++;
+        }
     }
 #endif
     
     /* For classification, return mode of top 5 */
+    printf("Shortlisted sample_choice...\n");
     int favor_cnt = 0;
     int unfavor_cnt = 0;
     for (int i = 0; i < K; i++) {
+        printf("%d: Choice: %d\n", sample_index[i], sample_choice[sample_index[i]]);
         if (sample_choice[sample_index[i]])
             favor_cnt++;
         else
--- assignment_001/job-classify.sh
+++ assignment_001/job-classify.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-export OverrideDefaultFP64Settings=1 
-export IGC_EnableDPEmulation=1 
-
-
-echo
-echo start: $(date "+%H%M%S.%3N")
-echo
-
-./knn-classify -d gpu
-
-echo
-echo stop:  $(date "+%H%M%S.%3N")
-echo
+./knn-classify -d cpu -p 28
